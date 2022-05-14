cd26bc0d853e10ceaf65d75ca9781e5330099e33
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-15 02:32:45 +0300

    second deployment

--- config.py
+++ config.py
@@ -5,13 +5,16 @@ import os
 class Config:
     SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
-    pass
+    
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+    
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
         SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
-    pass
+    
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:Access@localhost/pitchapp'
         
