f6c5b05610d68c253a631e71b6a6848f0539d987
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-14 05:09:06 +0300

    add columns to our user and pitch models; create comment model

--- app/models.py
+++ app/models.py
@@ -6,6 +6,12 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255), nullable = False, unique = True)
     pitches = db.relationship("Pitch", backref = 'user', lazy = 'dynamic')
+    email = db.Column(db.String(255), unique = True, nullable = False)
+    pass_secure = db.Column(db.String(128), unique = True, nullable = False)
+    bio = db.Column(db.String())
+    upvotes = db.relationship('Upvote', backref = 'user', lazy = 'dynamic')
+    comments = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
+    downvotes = db.relationship('Downvote', backref = 'user', lazy = 'dynamic')
     
     def __repr__(self):
         return f'User {self.username}'
@@ -17,6 +23,22 @@ class Pitch(db.Model):
     title = db.Column(db.String(255), nullable = False)
     pitch = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    category = db.Column(db.String)
+    upvotes = db.relationship('Upvote', backref='pitch', lazy = 'dynamic')
+    downvotes = db.relationship('Downvote', backref='pitch', lazy = 'dynamic')
+    comments = db.relationship('Comment', backref = 'pitch', lazy = 'dynamic')
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f'User {self.pitch} {self.category}'
+    
+    
+class Comment(db.Model):
+    __tablename__='comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+     
+    def __repr__(self):
+        return f'Comment  {self.title} {self.comment}'
