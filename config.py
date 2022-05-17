c58826cab7e7d9372060aefc7d9dec6fe51b595a
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-17 13:00:06 +0300

    change the sqlalchemy value in config classes

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
     SECRET_KEY='\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
     
-    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
     
     #SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:Access@localhost/pitchapp'
      
@@ -23,13 +23,13 @@ class Config:
     
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
         SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
     DEBUG=True
     
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
         
     DEBUG=True
 
