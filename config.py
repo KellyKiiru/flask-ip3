6dd23ed8ceff246d0ffc4732da1fc4c223bc579b
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-17 11:31:38 +0300

    patch: SQLALCHEMY_DATABASE_URI in prod and devconfig

--- config.py
+++ config.py
@@ -6,12 +6,14 @@ class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
     
     SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+    
+    #SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:Access@localhost/pitchapp'
      
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
         SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
         
     SQLALCHEMY_TRACK_MODIFICATIONS=True
-    
+
     MAIL_SERVER='smtp.googlemail.com'
     MAIL_PORT=465
     MAIL_USE_TLS=True
@@ -20,17 +22,17 @@ class Config:
     
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:Access@localhost/pitchapp'
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
         SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
-    DEBUG = True
+    DEBUG=True
     
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:Access@localhost/pitchapp'
         
-    DEBUG = True
+    DEBUG=True
 
-config_options = {
+config_options={
     'development': DevConfig,
     'production': ProdConfig,
 }
