f78ce2394dadd407f078ae16337be675f810995a
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-17 10:34:04 +0300

    set environments in class config

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 import os
+from dotenv import load_dotenv, find_dotenv
 
-
-
+load_dotenv(find_dotenv())
 class Config:
-    SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
+    SECRET_KEY=os.environ.get('SECRET_KEY')
     
-    #SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL')
-    
-    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI') 
+    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+     
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
         SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
         
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MAIL_USERNAME='kelly.kiiru@student.moringaschool.com'
 export MAIL_PASSWORD='@Kiiru2022'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://ulqtwvmlczkizk:3957faaea8dcffe77dd43dca8fb2ccc4c052ef61b55d8c37fd29dfdb51843ac5@ec2-52-86-115-245.compute-1.amazonaws.com:5432/dfhgfvb17oj1sh'
+export SECRET_KEY = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
 
 python3.8 manage.py server
